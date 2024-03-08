182fb94cb0c9e5bad0d1afbb634eb0b31af92659
namkhanhhhh <142679395+namkhanhhhh@users.noreply.github.com>
2024-03-08 12:47:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em Đào ngáo';
+const giftUrl = '';
+const eventName = 'Mở đi khẩn trương';
+const titleCard = 'Made by Khánhhhh';
+const contentCard = 'Chúc bạn Huyền của tớ hôm nay có thật nhiều niềm vui và hạnh phúc nhé. Mong mỗi ngày Huyền sẽ luôn được vui vẻ và yêu đời thế này. Mãi là người phụ nữ đẹp nhất trần gian ^^. Wish u all the best in 8/3!!!!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Huyenxinh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
